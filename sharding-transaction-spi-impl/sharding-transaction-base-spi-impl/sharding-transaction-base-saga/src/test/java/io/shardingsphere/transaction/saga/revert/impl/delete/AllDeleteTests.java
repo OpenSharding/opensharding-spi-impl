f1add3adf8400f5d7d57b5b9154ccee3b0fb8dd7
cherrylzhao <zhaojunv3@126.com>
2019-04-18 22:47:24 +0800

    remove unused DeleteRevertSQLGenerator & Maker.

--- sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/impl/delete/AllDeleteTests.java
+++ sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/impl/delete/AllDeleteTests.java
@@ -24,7 +24,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     DeleteRevertSQLExecuteWrapperTest.class,
-    RevertDeleteGeneratorTest.class,
     RevertDeleteTest.class
 })
 public class AllDeleteTests {
