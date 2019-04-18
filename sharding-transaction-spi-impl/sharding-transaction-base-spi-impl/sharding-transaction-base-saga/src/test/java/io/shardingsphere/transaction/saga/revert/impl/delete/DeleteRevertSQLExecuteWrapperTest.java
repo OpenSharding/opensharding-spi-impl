a1239a62f31a92dd72942f2cebac49b4d70dad5e
cherrylzhao <zhaojunv3@126.com>
2019-04-18 19:37:50 +0800

    add DeleteRevertSQLExecuteWrapperTest#assertGenerateRevertSQL

--- sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/impl/delete/DeleteRevertSQLExecuteWrapperTest.java
+++ sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/impl/delete/DeleteRevertSQLExecuteWrapperTest.java
@@ -17,7 +17,10 @@
 
 package io.shardingsphere.transaction.saga.revert.impl.delete;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import io.shardingsphere.transaction.saga.revert.api.DMLSnapshotAccessor;
+import io.shardingsphere.transaction.saga.revert.api.RevertSQLUnit;
 import io.shardingsphere.transaction.saga.revert.api.SnapshotSQLStatement;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,9 +29,14 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -43,22 +51,42 @@ public class DeleteRevertSQLExecuteWrapperTest {
     
     private DeleteRevertSQLExecuteWrapper deleteRevertSQLExecuteWrapper;
     
+    private List<Map<String, Object>> undoData = Lists.newLinkedList();
+    
     @Before
     public void setUp() {
-        when(snapshotSQLStatement.getActualTableName()).thenReturn("t_order");
+        when(snapshotSQLStatement.getActualTableName()).thenReturn("t_order_0");
         when(snapshotAccessor.getSnapshotSQLStatement()).thenReturn(snapshotSQLStatement);
         deleteRevertSQLExecuteWrapper = new DeleteRevertSQLExecuteWrapper(snapshotAccessor);
+        addUndoData();
+    }
+    
+    private void addUndoData() {
+        for (int i = 1; i <= 10; i++) {
+            Map<String, Object> record = new HashMap<>();
+            record.put("order_id", i);
+            record.put("user_id", i);
+            record.put("status", "init");
+            undoData.add(record);
+        }
     }
     
     @Test
     public void assertCreateRevertSQLContext() throws SQLException {
         DeleteRevertSQLContext actual = deleteRevertSQLExecuteWrapper.createRevertSQLContext();
-        assertThat(actual.getActualTable(), is("t_order"));
+        assertThat(actual.getActualTable(), is("t_order_0"));
         verify(snapshotAccessor).queryUndoData();
     }
     
     @Test
     public void assertGenerateRevertSQL() {
+        DeleteRevertSQLContext revertSQLContext = mock(DeleteRevertSQLContext.class);
+        when(revertSQLContext.getActualTable()).thenReturn("t_order_0");
+        when(revertSQLContext.getUndoData()).thenReturn(undoData);
+        Optional<RevertSQLUnit> actual = deleteRevertSQLExecuteWrapper.generateRevertSQL(revertSQLContext);
+        assertTrue(actual.isPresent());
+        assertThat(actual.get().getRevertSQL(), is("INSERT INTO t_order_0 VALUES (?,?,?)"));
+        assertThat(actual.get().getRevertParams().size(), is(10));
+        assertThat(actual.get().getRevertParams().iterator().next().size(), is(3));
     }
-
 }
