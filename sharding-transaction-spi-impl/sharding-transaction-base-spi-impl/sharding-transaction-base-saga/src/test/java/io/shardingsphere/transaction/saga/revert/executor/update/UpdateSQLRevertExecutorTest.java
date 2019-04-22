de94f1b631b30951ea488b600ccede0dc1b452d1
cherrylzhao <zhaojunv3@126.com>
2019-04-22 15:38:04 +0800

    add UpdateSQLRevertExecutorTest#assertFillParametersWithSetUpdatePrimaryKey

--- sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/executor/update/UpdateSQLRevertExecutorTest.java
+++ sharding-transaction-spi-impl/sharding-transaction-base-spi-impl/sharding-transaction-base-saga/src/test/java/io/shardingsphere/transaction/saga/revert/executor/update/UpdateSQLRevertExecutorTest.java
@@ -19,6 +19,7 @@ package io.shardingsphere.transaction.saga.revert.executor.update;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import io.shardingsphere.transaction.saga.revert.RevertSQLResult;
 import io.shardingsphere.transaction.saga.revert.executor.SQLRevertExecutorContext;
 import io.shardingsphere.transaction.saga.revert.snapshot.UpdateSnapshotAccessor;
 import org.apache.shardingsphere.core.parse.antlr.sql.statement.dml.UpdateStatement;
@@ -27,6 +28,7 @@ import org.apache.shardingsphere.core.parse.old.parser.expression.SQLExpression;
 import org.apache.shardingsphere.core.parse.old.parser.expression.SQLNumberExpression;
 import org.apache.shardingsphere.core.parse.old.parser.expression.SQLPlaceholderExpression;
 import org.apache.shardingsphere.core.parse.old.parser.expression.SQLTextExpression;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,6 +36,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -58,6 +61,8 @@ public class UpdateSQLRevertExecutorTest {
     @Mock
     private UpdateStatement updateStatement;
     
+    private RevertSQLResult revertSQLResult = new RevertSQLResult("");
+    
     private Map<Column, SQLExpression> updateAssignments = new LinkedHashMap<>();
     
     private List<Object> parameters = new LinkedList<>();
@@ -68,46 +73,72 @@ public class UpdateSQLRevertExecutorTest {
     
     @Before
     public void setUp() throws SQLException {
-        addUpdateAssignment("t_order", "status", new SQLTextExpression("init"));
-        addUpdateAssignment("t_order", "user_id", new SQLNumberExpression(22L));
-        addUpdateAssignment("t_order", "order_id", new SQLPlaceholderExpression(parameters.size()));
-        parameters.add(12345L);
-        addUndoData(10, "user_id", "status");
         when(updateStatement.getAssignments()).thenReturn(updateAssignments);
         when(snapshotAccessor.queryUndoData()).thenReturn(undoData);
         when(executorContext.getSqlStatement()).thenReturn(updateStatement);
         when(executorContext.getParameters()).thenReturn(parameters);
         when(executorContext.getActualTableName()).thenReturn("t_order_0");
         when(executorContext.getPrimaryKeyColumns()).thenReturn(Lists.newLinkedList(Collections.singleton("order_id")));
-        sqlRevertExecutor = new UpdateSQLRevertExecutor(executorContext, snapshotAccessor);
     }
     
-    private void addUndoData(final int count, String... columns) {
+    private void setUpdateAssignments(final String tableName, String... columns) {
+        for (String each : columns) {
+            Column column = new Column(each, tableName);
+            if ("status".equals(each)) {
+                updateAssignments.put(column, new SQLTextExpression("modified"));
+            } else if ("user_id".equals(each)) {
+                updateAssignments.put(column, new SQLNumberExpression(1L));
+            } else if ("order_id".equals(each)) {
+                updateAssignments.put(column, new SQLPlaceholderExpression(parameters.size()));
+                parameters.add(1000L);
+            }
+        }
+    }
+    
+    private void setSnapshot(final int count, String... columns) {
         for (int i = 1; i <= count; i++) {
             Map<String, Object> record = new HashMap<>();
             for (String each : columns) {
-                record.put(each, i);
+                record.put(each, each + "_" + i);
             }
             undoData.add(record);
         }
     }
     
-    private void addUpdateAssignment(final String tableName, final String columnName, final SQLExpression sqlExpression) {
-        Column column = new Column(columnName, tableName);
-        updateAssignments.put(column, sqlExpression);
-    }
-    
     @Test
-    public void revertSQL() {
+    public void assertRevertSQLWithSetUpdatePrimaryKey() throws SQLException {
+        setUpdateAssignments("t_order", "user_id", "status", "order_id");
+        setSnapshot(10, "user_id", "status", "order_id");
+        sqlRevertExecutor = new UpdateSQLRevertExecutor(executorContext, snapshotAccessor);
         Optional<String> actual = sqlRevertExecutor.revertSQL();
         assertTrue(actual.isPresent());
-        assertThat(actual.get(), is("UPDATE t_order_0 SET status = ?, user_id = ?, order_id = ? WHERE order_id = ?"));
-        
+        assertThat(actual.get(), is("UPDATE t_order_0 SET user_id = ?, status = ?, order_id = ? WHERE order_id = ?"));
     }
     
     @Test
-    public void fillParameters() {
+    public void assertRevertSQLWithoutSetUpdatePrimaryKey() throws SQLException {
+        setUpdateAssignments("t_order", "user_id", "status");
+        setSnapshot(10, "user_id", "status", "order_id");
+        sqlRevertExecutor = new UpdateSQLRevertExecutor(executorContext, snapshotAccessor);
+        Optional<String> actual = sqlRevertExecutor.revertSQL();
+        assertTrue(actual.isPresent());
+        assertThat(actual.get(), is("UPDATE t_order_0 SET user_id = ?, status = ? WHERE order_id = ?"));
     }
     
-    
+    @Test
+    public void assertFillParametersWithSetUpdatePrimaryKey() throws SQLException {
+        setUpdateAssignments("t_order", "user_id", "status", "order_id");
+        setSnapshot(1, "user_id", "status", "order_id");
+        sqlRevertExecutor = new UpdateSQLRevertExecutor(executorContext, snapshotAccessor);
+        sqlRevertExecutor.fillParameters(revertSQLResult);
+        assertThat(revertSQLResult.getParameters().size(), is(1));
+        for (Collection<Object> each : revertSQLResult.getParameters()) {
+            assertThat(each.size(), is(4));
+            List<Object> parameterRow = Lists.newArrayList(each);
+            assertThat(parameterRow.get(0), CoreMatchers.<Object>is("user_id_1"));
+            assertThat(parameterRow.get(1), CoreMatchers.<Object>is("status_1"));
+            assertThat(parameterRow.get(2), CoreMatchers.<Object>is("order_id_1"));
+            assertThat(parameterRow.get(3), CoreMatchers.<Object>is(1000L));
+        }
+    }
 }
